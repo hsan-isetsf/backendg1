dcda5432ad88754860cd91b307bfe9bc86423022
hsan.isetsf <hsan.isetsf@gmail.com>
2024-10-02 15:06:11 +0200

    first

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ScategorieController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('api')->group(function () {
+    Route::resource('categories', CategorieController::class);
+    });
+Route::middleware('api')->group(function () {
+    Route::resource('scategories', ScategorieController::class);
+});
+Route::get("/listarticles/{idscat}",[ArticleController::class,"showArticlesBySCAT"])->name("");
+
+Route::get('/articles/art/articlespaginate', [ArticleController::class, 'articlesPaginate']);
+/*Route::get("/categories",[CategorieController::class,"index"]);
+Route::post("/categories",[CategorieController::class,"store"]);
+Route::get("/categories/{id}",[CategorieController::class,"show"]);
+Route::delete("/categories/{id}",[CategorieController::class,"destroy"]);
+Route::put("/categories/{id}",[CategorieController::class,"update"]);*/
